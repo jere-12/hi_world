1a845fa432fa7832e7079e3421d52dc89c086b76
jere-12 <jeremiastorres2712@gmail.com>
2024-08-13 20:10:22 -0300

    hi_world

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] argentina) {
+
+        //escribir en ingles sino el indio no entiende.
+        for (int i = 0; i < 3; i++)
+        {
+        System.out.println("Hi world!");
+    }
+        System.out.println("Good bye world!");
+}
+}
